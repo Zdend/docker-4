fd681c084e30596cb0d1a824a2dc6eab6e1ce41e
Zdenek Vecek <8145571+Zdend@users.noreply.github.com>
2020-05-22 11:13:23 +1000

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,9 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders name', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Zdenek/i);
+  expect(linkElement).toBeInTheDocument();
+});
